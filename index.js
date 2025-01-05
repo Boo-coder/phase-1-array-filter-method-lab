18118709bce6f6d329d335d61270b59e7065c4aa
Boo-coder <Bouna112@yahoo.com>
2025-01-05 00:34:07 -0500

    filter

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+const matchDrivers = findMatching(drivers, "Bobby")
+
+console.log(matchDrivers);
+
+
+function fuzzyMatch(drivers, word){
+    return drivers.filter(driver => driver.toLowerCase().substring(0, word.length) === word.toLowerCase());
+}
+ const firstLetter = fuzzyMatch(drivers, "Sa");
+
+console.log(firstLetter);
+
+
+function matchName(drivers, match){
+    return drivers.filter(drivers => drivers.name === match)
+}
